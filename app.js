d0bb582e7b6411d461a801d182a2ac0a0305fe65
sasha sims <solely.sashasims@gmail.com>
2022-12-03 21:57:29 -0500

    squashed all bugs

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var form = document.getElementByID("arline-form");
-var submit = document.getElementByID(submit);
-var query = document.querySelector;
+let form = document.getElementById("airlineForm");
+let submit = document.getElementById("submit");
+// let query = document.querySelector;
 
-function formAlert{} {
-    var firstName = form.elements["firstName"].value;
-    var lastName = form.elements["lastName"].value;
-    var age = form.elements["age"].value;
-    var gender = form.elements["gender"].value;
-    var location = form.elements["travel-location"].value;
-    var diet = {};
+function formAlert() {
+    let firstName = form.elements["first-name"].value;
+    let lastName = form.elements["last-name"].value;
+    let age = form.elements["age"].value;
+    let gender = form.elements["gender"].value;
+    let location = form.elements["travel-location"].value;
+    console.log(location)
+    let diet = [];
     if (form.elements['vegan'].checked) {
-        var diet.pop(document.getElementById("vegan").value);
+        diet.push(document.getElementById("vegan").value);
     }
     if (form.elements['gluten'].checked) {
         diet.push(document.getElementById('gluten').value);
@@ -24,4 +25,4 @@ function formAlert{} {
 }
 
 
-submit.addEventListener("click", formalert);
+submit.addEventListener("click", formAlert)
